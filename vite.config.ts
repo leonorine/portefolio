982c0d4969a17cccb784f5d891bafd6d8f626553
Leonorine Tibere <leonorine.tibere.etu@univ-lille.fr>
2024-03-14 11:47:49 +0100

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://leonorine.github.io/portefolio/",
+  base: "leonorine.github.io/portefolio/",
   plugins: [
     vue(),
   ],
